84d15d390d7911f8bd0a382a33c2233a9bc99b30
Victor José Arana Rodríguez <victor_arana@hotmail.com>
2014-04-24 11:04:12 -0500

    Spring Tutorial 07.2

--- src/main/java/com/victorarana/spring/Triangle.java
+++ src/main/java/com/victorarana/spring/Triangle.java
@@ -2,36 +2,35 @@ package com.victorarana.spring;
 
 public class Triangle {
 	
-	private String type;
-	private int height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	public int getHeight() {
-		return height;
-	}
-	
-	public Triangle(String type){
-		this.type = type;
-	}
-	
-	public Triangle(int height){
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height){
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
+	public Point getPointA() {
+		return pointA;
 	}
 
-//	public void setType(String type) {
-//		this.type = type;
-//	}
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 	public void draw(){
-		System.out.println(getType() + " " + "Triangle drawn of height " + getHeight());
+		
 	}
-
 }
