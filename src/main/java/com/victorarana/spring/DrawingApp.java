75585ce9c8cbf8e27b821ea64dfdb11b2ae64509
Victor Arana <victor_arana@hotmail.com>
2014-04-24 23:51:13 -0500

    Spring Tutorial 09 - Initializing Collections

--- src/main/java/com/victorarana/spring/DrawingApp.java
+++ src/main/java/com/victorarana/spring/DrawingApp.java
@@ -12,7 +12,7 @@ public class DrawingApp {
 //		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 		 @SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("/spring.xml");
-		Triangle triangle = (Triangle) context.getBean("triangle-alias");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 }
--- src/main/java/com/victorarana/spring/Triangle.java
+++ src/main/java/com/victorarana/spring/Triangle.java
@@ -1,38 +1,22 @@
 package com.victorarana.spring;
 
+import java.util.List;
+
 public class Triangle {
 	
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-	
-	public Point getPointA() {
-		return pointA;
+	private List<Point>	points;
+
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
 	public void draw(){
-		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
-		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
-		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+		for(Point point: points){
+			System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
+		}
 	}
 }
